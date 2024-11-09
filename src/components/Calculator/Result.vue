028e6c40eebf5987a407917a83234cf6faf585f7
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2024-11-09 11:47:40 +0530

    Break the design in to components

---
+++ src/components/Calculator/Result.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="">
+    </div>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'Result',
+        props: {},
+    });
+<script>
+<style scoped>
+</style>
--- src/components/__tests__/example.test.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from 'vitest'
-
-function sum(a, b) {
-    return a + b;
-}
-
-test("add two numbers", () => {
-    expect(sum(2, 3)).toEqual(5)
-})
